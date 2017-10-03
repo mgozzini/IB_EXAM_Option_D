0f720a9b23478fa36bff0ce4f839e66b4ee728e7
h205p2 <matteogozzini@students.berkeley.net>
2017-10-03 09:56:52 -0700

    Initial commit

---
+++ src/RollingStock.java
@@ -0,0 +1,23 @@
+
+public class RollingStock{
+    private int mIDNumber;
+    private double mWeight;
+
+    public RollingStock(int ID, double weight){
+        mIDNumber = ID;
+        mWeight = weight; // Weight is in kilograms
+    }
+
+    // Accessor methods
+    public double getWeight(){
+        return mWeight;
+    }
+
+    public int getID(){
+        return mIDNumber;
+    }
+
+// Other methods
+
+}
+
---
+++ src/Train.java
@@ -0,0 +1,43 @@
+
+public class Train {
+    private Engine[] mEngines;
+    private Wagon[] mWagons;
+    private int mEngineCount;
+    private int mWagonCount;
+    private int mTrainNumber;
+    private double mWeight; // Total weight in kilograms
+
+    public Train(int number){
+        mTrainNumber = number;
+        mEngines = new Engine[6]; // The train can have up to 6 engines
+        mEngineCount = 0;
+        mWagons = new Wagon[100]; // The train can have up to 100 wagons
+        mWagonCount = 0;
+        mWeight = 0;
+    }
+
+    public void addEngine( Engine newEngine ){
+        mEngines[mEngineCount] = newEngine;
+        mEngineCount++;
+    }
+
+    public Engine removeEngine(){
+        mEngineCount--;
+        return mEngines[mEngineCount];
+    }
+
+    public void addWagon( Wagon newWagon ){
+        mWagons[mWagonCount] = newWagon;
+        mWagonCount++;
+    }
+
+    public Wagon removeWagon(){
+        // Code to be written
+    }
+
+    public double getWeight(){
+        // Code to be written
+    }
+
+}
+
